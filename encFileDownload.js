65032498c394c885a9d90433b65b74de609947e2
Brandon DeMatteis <brandon.dematteis@enclude.ie>
2022-02-07 16:01:05 +0100

    first commit

---
+++ encFileDownload.js
@@ -0,0 +1,16 @@
+import { LightningElement, api, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+
+export default class EncFileDownload extends LightningElement {
+   @wire(CurrentPageReference)
+   pageRef;
+
+   @api baseUrl
+   @api linkText = 'Download File';
+   @api fileId;
+
+   get href() {
+      return `/sfsites/c/cms/delivery/media/${this.fileId}`
+   }
+
+}
